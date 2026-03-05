cea9740af2f1979c6acacd192920c3621881188c
Sanyukta-Futane01 <sanyuktafutane01@gmail.com>
2026-03-06 00:35:33 +0530

    programs

--- Function1.py
+++ Function1.py
@@ -1,9 +1,8 @@
 # Function to convert sentence into capital letters
 def capitalize_sentence(text):
     print(text.upper())
-
 # Taking input from user
-line = input("Enter a sentence: ")
+line = input("Enter a sentence:  ")
 
 # Calling the function
 capitalize_sentence(line)
