9ac259b378347a6fec5b2757b33aa7a45524f6fd
Sanyukta-Futane01 <sanyuktafutane01@gmail.com>
2026-03-24 21:50:18 +0530

    program

--- PostLab_Project.py
+++ PostLab_Project.py
@@ -1,4 +1,8 @@
 # Smart Home Energy Consumption Monitor
+# The Challenge: Simulate a smart meter that reads voltage (V) and resistance (R)
+# for various home appliances. Calculate current (I) and Power (P). Use Control
+# Flow to identify high-power appliances and File Handling to generate a "Daily
+# Consumption Report" that suggests ways to save electricity.
 
 def calculate_current(voltage, resistance):
     return voltage / resistance
@@ -55,3 +59,54 @@ print(content)
 read_file.close()
 
 print("Report Displayed Successfully!")
+
+
+
+
+
+# ------------------------------------------------OUTPUT:------------------------------------------------
+
+
+# Enter number of appliances: 2
+
+# Enter Appliance Name: Fan
+# Enter Voltage (V): 29
+# Enter Resistance (Ohms): 200
+
+# Appliance: Fan
+# Current: 0.14 A
+# Power: 4.21 W
+
+# Enter Appliance Name: AC
+# Enter Voltage (V): 20
+# Enter Resistance (Ohms): 300
+
+# Appliance: AC
+# Current: 0.07 A
+# Power: 1.33 W
+
+
+
+# ------ Daily Consumption Report ------
+# Appliance: Fan
+# Voltage: 29.0 V
+# Resistance: 200.0 Ohms
+# Current: 0.14 A
+# Power: 4.21 W
+# Status: Normal Power Appliance
+
+# Appliance: AC
+# Voltage: 20.0 V
+# Resistance: 300.0 Ohms
+# Current: 0.07 A
+# Power: 1.33 W
+# Status: Normal Power Appliance
+
+
+# General Energy Saving Tips:
+# - Turn off appliances when not in use.
+# - Use LED bulbs.
+# - Use energy-efficient appliances.
+# - Avoid overloading circuits.
+
+# Report Displayed Successfully!
